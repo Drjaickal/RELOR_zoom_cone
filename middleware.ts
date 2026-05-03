66637e01e167f5666084a0c5be0f7ae14908e34c
Drjaickal <105309360+Drjaickal@users.noreply.github.com>
2026-05-03 18:55:28 +0530

    fixed the middleware issue 2

--- middleware.ts
+++ middleware.ts
@@ -1,20 +1,27 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
 const protectedRoutes = createRouteMatcher([
-  '/upcoming',
-  '/previous',
-  '/recordings',
-  '/personal-room',
-  '/meeting(.*)'
+  "/upcoming",
+  "/previous",
+  "/recordings",
+  "/personal-room",
+  "/meeting(.*)",
 ]);
 
 export default clerkMiddleware((auth, req) => {
   if (protectedRoutes(req)) {
-    auth().protect();
+    const { userId } = auth();
+
+    if (!userId) {
+      return auth().redirectToSignIn();
+    }
   }
 });
 
-
 export const config = {
-  matcher: ["/((?!_next|.*\\..*).*)"],
+  matcher: [
+    "/((?!.+\\.[\\w]+$|_next).*)",
+    "/",
+    "/(api|trpc)(.*)",
+  ],
 };
