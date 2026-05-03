08320f5471927ba22101d2cef0ef923389fddd90
Drjaickal <105309360+Drjaickal@users.noreply.github.com>
2026-05-03 16:56:51 +0530

    "fix middleware crash"

--- middleware.ts
+++ middleware.ts
@@ -1,18 +1,19 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
 const protectedRoutes = createRouteMatcher([
-    '/',
-    '/upcoming',
-    '/previous',
-    '/recordings',
-    '/personal-room',
-    '/meeting(.*)'
-])
+  '/upcoming',
+  '/previous',
+  '/recordings',
+  '/personal-room',
+  '/meeting(.*)'
+]);
 
 export default clerkMiddleware((auth, req) => {
-    if(protectedRoutes(req)) auth().protect();
+  if (protectedRoutes(req)) {
+    auth().protect();
+  }
 });
 
 export const config = {
-  matcher: ['/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)'],
+  matcher: ["/((?!_next|.*\\..*).*)"],
 };
